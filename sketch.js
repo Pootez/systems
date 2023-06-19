92050d76a2d1d234c9d905980ffddb79fa8ffc01
Pootez <sondrenoe@gmail.com>
2023-06-19 11:30:09 +0200

    p5 implementation

---
+++ sketch.js
@@ -0,0 +1,14 @@
+const fr = 60
+
+function setup() {
+    createCanvas(windowWidth, windowHeight)
+    frameRate(fr)
+}
+
+function draw() {
+    background(0)
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight)
+}
